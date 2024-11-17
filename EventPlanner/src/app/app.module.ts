ffe25033e6e8a788f6bae433f1f56b60bacb2de2
David Stakic <davidstakic2003@gmail.com>
2024-11-17 17:09:40 +0100

    [ADD] GUI For Login Screen Implemented With Simple Validation

--- EventPlanner/src/app/app.module.ts
+++ EventPlanner/src/app/app.module.ts
@@ -3,17 +3,21 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideAnimationsAsync()
   ],
   bootstrap: [AppComponent]
 })
